28104167419d5a8f9298a07ab199a839b6291b57
Milena Fleming <milenasapucaia29@gmail.com>
2022-04-24 12:43:21 +0100

    set up of eslint config and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+/*global module*/
+/*eslint no-undef: "error"*/
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['airbnb','prettier', 'react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  rules: {
+    indent: ['error', 'tab'],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'prettier/prettier': ['error'],
+  },
+};
---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+
+
